AOSPA/android_frameworks_base-old

WiredAccessoryManager: use dev index from UEventInfo to form devPath
DP extcon support does not always counts from index zero.
In checkDevIndex(), updating variable used to construct
dev path string, i.e. index, to dev_index so that
it also works in other device nodes.

CRs-Fixed: 2986549
Change-Id: Idb2ebfc18947681e9f8ca62bb8f7f5772b55b723

services/core/java/com/android/server/WiredAccessoryManager.java
@@ -624,7 +624,7 @@ final class WiredAccessoryManager implements WiredAccessoryCallbacks {
                 while (true) {
                     String devPath = String.format(Locale.US,
                           "/sys/devices/platform/soc/%s/extcon/extcon%d/name",
-                                                   NAME_DP_AUDIO, index);
+                                                   NAME_DP_AUDIO, dev_index);
                     if (LOG) {
                         Slog.v(TAG, "checkDevIndex " + devPath);
                     }

jackson177/EvolutionX-fwb

services/core/java/com/android/server/WiredAccessoryManager.java
@@ -609,7 +609,7 @@ final class WiredAccessoryManager implements WiredAccessoryCallbacks {
                 while (true) {
                     String devPath = String.format(Locale.US,
                             "/sys/devices/platform/soc/%s/extcon/extcon%d/name",
-                            NAME_DP_AUDIO, index);
+                            NAME_DP_AUDIO, devIndex);
                     if (LOG) {
                         Slog.v(TAG, "checkDevIndex " + devPath);
                     }